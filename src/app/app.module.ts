22b18b8a1b61e2b9ba8727a38a7cae71b8534350
Stas <holidayonmail@gmail.com>
2021-02-17 23:02:33 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,17 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WaringAlertComponent } from './warning-alert/waring-alert.component';
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SuccessAlertComponent,
+    WaringAlertComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/warning-alert/waring-alert.component.ts
+++ src/app/warning-alert/waring-alert.component.ts
@@ -1,7 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-waring-alert',
+  selector: 'app-warning-alert',
   template: ` <p>This is a waring alert</p> `,
+  styles: [
+    `
+      p {
+        padding: 20px;
+        background-color: mistyrose;
+        border: 1px solid red;
+      }
+    `,
+  ],
 })
 export class WaringAlertComponent {}
